2f6e59ed1742eb8f2614ea8478c09c30d00d21b3
Peter Spiess-Knafl <dev@spiessknafl.at>
2018-06-28 17:47:37 +0200

    /refactor-cmake: Auto stash before merge of "feature/refactor-cmake" and "origin/feature/refactor-cmake"

---
+++ src/jsonrpccpp/connector/microhttpd/httpserver_test.cpp
@@ -0,0 +1,113 @@
+#define CATCH_CONFIG_MAIN
+#include <curl/curl.h>
+#include <catch.hpp>
+
+#include <string>
+
+#include "TestClientConnectionHandler.h"
+#include "httpserver.h"
+
+using namespace jsonrpc;
+using namespace std;
+
+#define TEST_MODULE "[connector-microhttpd]"
+
+#define TEST_PORT 8383
+#define CLIENT_URL "http://localhost:8383"
+
+struct F {
+  HttpServer server;
+  TestClienctConnectionHandler handler;
+  F() : server(TEST_PORT) {
+    server.AddConnectionHandler(handler);
+    REQUIRE(server.StartListening() == true);
+  }
+
+  ~F() { server.StopListening(); }
+};
+
+TEST_CASE_METHOD(F, "server_start_stop", TEST_MODULE) {
+  REQUIRE(server.StartListening() == false);
+  REQUIRE(server.StopListening() == true);
+  REQUIRE(server.StopListening() == false);
+}
+
+TEST_CASE_METHOD(F, "http_get", TEST_MODULE) {
+  CURL *curl = curl_easy_init();
+
+  curl_easy_setopt(curl, CURLOPT_URL, CLIENT_URL);
+  curl_easy_setopt(curl, CURLOPT_NOBODY, 1);
+  CURLcode code = curl_easy_perform(curl);
+  REQUIRE(code == CURLE_OK);
+
+  long http_code = 0;
+  curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+  REQUIRE(http_code == 405);
+
+  curl_easy_cleanup(curl);
+}
+
+TEST_CASE_METHOD(F, "http_options", TEST_MODULE) {
+  CURL *curl = curl_easy_init();
+
+  curl_easy_setopt(curl, CURLOPT_URL, CLIENT_URL);
+  curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "OPTIONS");
+  CURLcode code = curl_easy_perform(curl);
+  REQUIRE(code == CURLE_OK);
+
+  long http_code = 0;
+  curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+  CHECK(http_code == 200);
+
+  curl_easy_cleanup(curl);
+}
+
+size_t WriteCallback(char *contents, size_t size, size_t nmemb, void *userp) {
+  ((std::string *)userp)->append((char *)contents, size * nmemb);
+  return size * nmemb;
+}
+
+string ExecutePostRequest(const string &url, const string &content) {
+  string response;
+  CURL *curl = curl_easy_init();
+
+  curl_easy_setopt(curl, CURLOPT_URL, CLIENT_URL);
+  curl_easy_setopt(curl, CURLOPT_POSTFIELDS, content.c_str());
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
+  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, false);
+
+  CURLcode result = curl_easy_perform(curl);
+
+  REQUIRE(result == CURLE_OK);
+  curl_easy_cleanup(curl);
+
+  return response;
+}
+
+TEST_CASE_METHOD(F, "http_post", TEST_MODULE) {
+  handler.response = "This is a microhttpd response";
+
+  string response = ExecutePostRequest(CLIENT_URL, "This is a curl request");
+
+  REQUIRE(handler.request == "This is a curl request");
+  REQUIRE(response == handler.response);
+}
+
+TEST_CASE("http_tls_invalid_startup", TEST_MODULE) {
+  HttpServer server(TEST_PORT, "/a/b/c", "/d/e/f");
+  REQUIRE(server.StartListening() == false);
+}
+
+TEST_CASE("http_tls_valid_startup", TEST_MODULE) {
+  HttpServer server(TEST_PORT, "server.pem", "server.key");
+  TestClienctConnectionHandler handler;
+  handler.response = "This is a microhttpd response";
+  server.AddConnectionHandler(handler);
+
+  REQUIRE(server.StartListening() == true);
+
+  REQUIRE(ExecutePostRequest(CLIENT_URL, "This is a curl request") == handler.response);
+  server.StopListening();
+}
